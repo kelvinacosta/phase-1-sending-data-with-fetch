1fa5a5c593c0b560ee6cd957c9a42213a0478688
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-04-22 13:00:32 -0400

    complete POST fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+ function submitData (name,email){
+    
+    const formData = {
+        name,
+        email
+    }
+    
+    const configurationObj = {
+        method:"POST",
+        headers: {
+            "content-type": "application/json",
+            "accept": "application/json"
+          },
+        body: JSON.stringify(formData),
+        
+    }
+    
+    return fetch('http://localhost:3000/users',configurationObj)
+    .then(res => res.json())
+    .then(data => {
+        const id = data.id
+        document.body.append(id)
+    })
+    .catch(error => {
+        const errorMessage = error.message;
+        document.body.append(errorMessage)
+    })
+}
